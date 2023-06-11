08df677982503c10b2abe951bcf4af48f912c561
良原 貴弘 <taka03vivi@gmail.com>
2023-06-11 10:05:20 +0900

    メッセージのリスト化と展開アニメーション追加

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -4,21 +4,29 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
@@ -32,9 +40,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeTutorialTheme{
-                Surface(modifier = Modifier.fillMaxSize()) {
-                    MessageCard(Message("Android","Jetpack"))
-                }
+                Conversation(messages = SampleData.conversationSample)
             }
         }
     }
@@ -55,23 +61,51 @@ fun MessageCard(msg : Message){
                 .border(1.5.dp, MaterialTheme.colorScheme.secondary, CircleShape)
         )
         Spacer(modifier = Modifier.width(8.dp))
-        Column {
+
+        // メッセージの展開状態を管理する変数
+        var isExpanded by remember { mutableStateOf(false) }
+        // メッセージの展開状態で背景色を切り替える
+        val surfaceColor by animateColorAsState(
+            if(isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface
+        )
+
+        // このColumnをクリックすると、isExpanded変数のOn/Offの切り替え。
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
             Text(
                 text = msg.author,
                 color = MaterialTheme.colorScheme.secondary,
                 style = MaterialTheme.typography.titleMedium
             )
             Spacer(modifier = Modifier.height(4.dp))
-            Surface(shape = MaterialTheme.shapes.medium, tonalElevation = 1.dp) {
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                tonalElevation = 1.dp,
+                color = surfaceColor,
+                // animateContentSizeは、Surfaceのサイズを徐々に変化させます。
+                modifier = Modifier.animateContentSize().padding(1.dp)
+            ) {
                 Text(
                     text = msg.body,
                     modifier = Modifier.padding(all = 4.dp),
+                    // メッセージが展開状態のときは全て表示、それ以外は１行のみ表示
+                    maxLines = if(isExpanded) Int.MAX_VALUE else 1,
                     style = MaterialTheme.typography.bodyMedium
                 )
 
             }
         }
     }
+}
+
+@Composable
+fun Conversation(messages: List<Message>){
+
+    LazyColumn{
+        items(messages){message ->
+            MessageCard(msg = message)
+        }
+    }
+
 }
 
 // プレビューでレイアウトを確認するときは
@@ -89,5 +123,13 @@ fun PreviewMessageCard(){
             MessageCard(Message("Author","Body"))
         }
         
+    }
+}
+
+@Preview
+@Composable
+fun PreviewConversation(){
+    ComposeTutorialTheme {
+        Conversation(messages = SampleData.conversationSample)
     }
 }
