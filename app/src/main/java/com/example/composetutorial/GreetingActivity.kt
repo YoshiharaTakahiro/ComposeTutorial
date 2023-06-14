11415d296d8b3d26ec28ec7c3a19a2861c4590d8
良原 貴弘 <taka03vivi@gmail.com>
2023-06-15 00:46:50 +0900

    コードラボ(Jetpack Composeの基本)完了

--- app/src/main/java/com/example/composetutorial/GreetingActivity.kt
+++ app/src/main/java/com/example/composetutorial/GreetingActivity.kt
@@ -1,25 +1,39 @@
 package com.example.composetutorial
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.material3.Button
-import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composetutorial.ui.theme.ComposeTutorialTheme
@@ -38,7 +52,7 @@ class GreetingActivity : ComponentActivity() {
 @Composable
 fun MyApp(modifier: Modifier = Modifier){
     // OnboardingScreenで使用する値をホイスティングするため共有部に移動
-    var shouldShowOnboarding by remember { mutableStateOf(true) }
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
 
     Surface(modifier) {
         if(shouldShowOnboarding){
@@ -61,10 +75,13 @@ fun MyAppPreview() {
 @Composable
 private fun Greetings(
     modifier: Modifier = Modifier,
-    names: List<String> = listOf("Wolrd", "Compose")
+    names: List<String> = List(1000) { "$it" }
 ){
-    Column(modifier = modifier.padding(vertical = 4.dp)) {
-        for(name in names){
+
+    // LazyColumn は画面に表示されているアイテムのみをレンダリングする
+    LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
+
+        items(items = names){ name ->
             Greeting(name = name)
         }
     }
@@ -72,33 +89,74 @@ private fun Greetings(
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Card(
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primary
+        ),
+        modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ){
+        CardContent(name = name, modifier = modifier)
+    }
+}
+
+@Composable
+fun CardContent(name:String, modifier: Modifier = Modifier){
 
     // 再コンポジションの前後で状態を保持するには、remember を使用して可変状態を「記憶」しておく
-    val expanded = remember{ mutableStateOf(false)}
-    val extraPadding = if(expanded.value) 48.dp else 0.dp
+    var expanded by rememberSaveable{ mutableStateOf(false)}
 
-    Surface(
-        color = MaterialTheme.colorScheme.primary,
-        modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
-    ) {
-        Row(modifier = modifier.padding(24.dp)){
-            Column(
-                modifier = modifier
-                    .weight(1f)
-                    .padding(bottom = extraPadding)
-            ) {
-                Text(text = "Hello,")
-                Text("$name!")
+    Row(
+        modifier = modifier
+            .padding(12.dp)
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
+            )
+    ){
+        Column(
+            modifier = modifier
+                .weight(1f)
+                .padding(12.dp)
+        ) {
+            Text(text = "Hello,")
+            Text(
+                "$name!",
+                style = MaterialTheme.typography.headlineMedium.copy(
+                    fontWeight = FontWeight.ExtraBold
+                )
+            )
+            // 展開時のみ表示
+            if(expanded){
+                Text(
+                    text = ("Composem ipsum color sit lazy, " +
+                            "padding theme elit, sed do bouncy. ").repeat(4),
+                )
             }
-            ElevatedButton(
-                onClick = { expanded.value = !expanded.value }
-            ) {
-                Text(if(expanded.value) "Show less" else "Show more")
-            }
+        }
+        // アイコンに変更
+        IconButton(
+            onClick = { expanded = !expanded}
+        ) {
+            Icon(
+                imageVector = if(expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                contentDescription = if(expanded){
+                    stringResource(R.string.show_less)
+                }else{
+                    stringResource(R.string.show_more)
+                }
+            )
         }
     }
 }
 
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "Dark"
+)
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 fun GreetingPreview() {
